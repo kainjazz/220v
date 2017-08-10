58bea4a8aa77b96c43f5bc2954aebbbdbe7ef4e1
stan <sts.ko@mail.ru>
2017-08-10 17:56:55 +0300

    Небольшое дополнение по тестовой странице - ~20 минут

--- view/howitworks.php
+++ view/howitworks.php
@@ -11,7 +11,7 @@
     <html lang="rus">
     <head>
         <meta charset="UTF-8">
-        <title>АстроЦентр Ландра</title>
+        <title>220v</title>
         <script
             src="http://code.jquery.com/jquery-3.2.1.min.js"
             integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
@@ -21,22 +21,56 @@
     <body>
         <script>
             function send() {
-                $.ajax({
-                    type:"PUT",
-                    url: 'index.php/api/method',
-                    data: {
-                        name: "Вася",
-                        lastname: "Пупкин"
-                    },
-                    success: function(msg){
-                        $("#ans").html(JSON.stringify(msg));
+                var m = $("input");
+                var method = "";
+                var title=$("input[name=title]").val(),
+                    vendor=$("input[name=vendor]").val(),
+                    price=$("input[name=price]").val(),
+                    short=$("input[name=short]").val();
+
+                for (i = 0; i < m.length; i++) {
+                    if(m[i].checked === true) {
+                        method = m[i].value;
                     }
-                });
+                }
+
+                if(method!=="") {
+                    $.ajax({
+                        type: method,
+                        url: '/api/method',
+                        data: {
+                            title:title,
+                            vendor:vendor,
+                            price:price,
+                            short:short
+                        },
+                        success: function (msg) {
+                            $("#ans").html(JSON.stringify(msg));
+                        }
+                    });
+                } else {
+                    $("#ans").html("Надо выбрать метод");
+                }
             }
         </script>
-        <div id="ans">
+        <input type="radio" name="method" value="POST">POST<br/>
+        <input type="radio" name="method" value="GET">GET<br/>
+        <input type="radio" name="method" value="PUT">PUT<br/>
+        <input type="radio" name="method" value="DELETE">DELETE<br/>
+        <p>
+            <input type="text" placeholder="Заголовок" name="title">
+            <input type="text" placeholder="Брэнд" name="vendor">
+            <input type="text" placeholder="Цена" name="price">
+            <input type="text" placeholder="Краткое описание" name="short">
+        </p>
+        <p><input type="text" name="text" value="/api/"></p>
+        <p>
+            <div id="ans">
 
-        </div>
-        <button onclick="send();">Отправить запрос</button>
+            </div>
+        </p>
+        <p>
+            <button onclick="send();">Отправить запрос</button>
+        </p>
     </body>
     </html>
